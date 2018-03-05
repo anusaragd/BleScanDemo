f5bd1a67d5936b79f9c0252cb9024fdd856a9400
MASTERS <anusara.orm@gmail.com>
2018-03-05 13:03:49 +0700

    Initial commit

---
+++ app/src/main/java/com/minewbeacon/blescan/demo/EditnameActivity.java
@@ -0,0 +1,17 @@
+package com.minewbeacon.blescan.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yuliwuli.blescan.demo.R;
+
+public class EditnameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editname);
+
+
+    }
+}
---
+++ app/src/main/java/com/minewbeacon/blescan/demo/splash.java
@@ -0,0 +1,47 @@
+package com.minewbeacon.blescan.demo;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+import com.yuliwuli.blescan.demo.R;
+
+public class splash extends AppCompatActivity {
+
+    Handler handler;
+    Runnable runnable;
+    long delay_time;
+    long time = 5000L;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash);
+
+        handler = new Handler();
+
+        runnable = new Runnable() {
+            public void run() {
+                Intent intent = new Intent(splash.this, EditnameActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+    }
+    public void onResume() {
+        super.onResume();
+        delay_time = time;
+        handler.postDelayed(runnable, delay_time);
+        time = System.currentTimeMillis();
+    }
+
+    public void onPause() {
+        super.onPause();
+        handler.removeCallbacks(runnable);
+        time = delay_time - (System.currentTimeMillis() - time);
+    }
+}
