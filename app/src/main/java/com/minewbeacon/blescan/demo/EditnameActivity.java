248201f548d766c2674633dbf2294ffa1e3d065c
MASTERS <anusara.orm@gmail.com>
2018-03-05 16:19:51 +0700

    go go up up yeahh!!

--- app/src/main/java/com/minewbeacon/blescan/demo/EditnameActivity.java
+++ app/src/main/java/com/minewbeacon/blescan/demo/EditnameActivity.java
@@ -1,16 +1,37 @@
 package com.minewbeacon.blescan.demo;
 
+import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.yuliwuli.blescan.demo.R;
 
 public class EditnameActivity extends AppCompatActivity {
 
+    Button Sub_edit ;
+    EditText name_edit ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editname);
+
+        name_edit = (EditText) findViewById(R.id.name_editText);
+        name_edit.setText(Build.MODEL);
+
+        Sub_edit = (Button)findViewById(R.id.button_editname);
+        Sub_edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(EditnameActivity.this,MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
 
 
     }
