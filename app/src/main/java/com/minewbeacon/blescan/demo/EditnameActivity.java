14aab4ce1611192599d69c048ce39fa7cb40659d
MASTERS <anusara.orm@gmail.com>
2018-03-06 14:14:48 +0700

    SQLite yeah

--- app/src/main/java/com/minewbeacon/blescan/demo/EditnameActivity.java
+++ app/src/main/java/com/minewbeacon/blescan/demo/EditnameActivity.java
@@ -1,6 +1,7 @@
 package com.minewbeacon.blescan.demo;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,17 +9,23 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.minewbeacon.blescan.demo.SQLite.myDBClass;
 import com.yuliwuli.blescan.demo.R;
 
 public class EditnameActivity extends AppCompatActivity {
 
     Button Sub_edit ;
     EditText name_edit ;
+    SQLiteDatabase db;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editname);
+
+        myDBClass myDb = new myDBClass(this);
+        myDb.getWritableDatabase(); // First method
 
         name_edit = (EditText) findViewById(R.id.name_editText);
         name_edit.setText(Build.MODEL);
--- app/src/main/java/com/minewbeacon/blescan/demo/SQLite/myDBClass.java
+++ app/src/main/java/com/minewbeacon/blescan/demo/SQLite/myDBClass.java
@@ -7,17 +7,34 @@ package com.minewbeacon.blescan.demo.SQLite;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 public class myDBClass extends SQLiteOpenHelper {
 
+    // Database Version
+    private static final int DATABASE_VERSION = 1;
 
-    public myDBClass(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    // Database Name
+    private static final String DATABASE_NAME = "mydatabase";
+
+    // Table Name
+    private static final String TABLE_MEMBER = "members";
+
+
+
+    public myDBClass(Context context) {
+        super(context,  DATABASE_NAME, null, DATABASE_VERSION);
     }
 
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL("CREATE TABLE members " +
+                "(MemberID INTEGER PRIMARY KEY," +
+                " Name TEXT(100));");
+
+        Log.d("CREATE TABLE","Create Table Successfully.");
 
     }
 
